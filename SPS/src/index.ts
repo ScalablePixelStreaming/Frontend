08bd62fa1906e97db1b30d4f93d01c48de7f9dbc
Luke Bermingham <1215582+lukehb@users.noreply.github.com>
2023-03-08 15:48:48 +1000

    Upgrade index.ts usage of lib-pixelstreaming API

--- SPS/src/index.ts
+++ SPS/src/index.ts
@@ -1,20 +1,26 @@
 import { SPSApplication } from "./SPSApplication";
 import { Config, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.2';
 import { PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.2';
-export const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
 
+// Apply default styling from Epic's frontend
+export const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
+PixelStreamingApplicationStyles.applyStyleSheet();
 
 document.body.onload = function () {
 
 	// Example of how to set the logger level
 	//libfrontend.Logger.SetLoggerVerbosity(10);
 
-	// Create a config object							// Extremely important, SPS only support browser sending the offer.
+	// Create a config object.
+	// Note: This config is extremely important, SPS only supports the browser sending the offer.
 	const config = new Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
 
 	// Create a Native DOM delegate instance that implements the Delegate interface class
 	const stream = new PixelStreaming(config);
-	const spsApplication = new SPSApplication({ stream });
+	const spsApplication = new SPSApplication({ 
+		stream,
+		onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode) /* Light/Dark mode support. */
+	});
 
 	document.body.appendChild(spsApplication.rootElement);
 }
