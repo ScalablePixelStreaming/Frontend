c2747ed1db331913d304725e85cbceb01eac6064
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-14 10:23:33 +1000

    moddified package.json and wepack files to accomadate the example

--- examples/defaultSpsExample/webpack.config.js
+++ examples/defaultSpsExample/webpack.config.js
@@ -1,64 +1,75 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
-const isProduction = process.env.NODE_ENV == 'production';
+module.exports = (env) => {
+    return {
+        mode: "development",
+        entry: './src/index.ts',
+        plugins: [
+            new webpack.DefinePlugin({
+                WEBSOCKET_URL: JSON.stringify((env.WEBSOCKET_URL !== undefined) ? env.WEBSOCKET_URL : '')
+            }),
 
-
-const stylesHandler = 'style-loader';
-
-
-
-const config = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: 'ts-loader',
-                exclude: ['/node_modules/'],
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+            new HtmlWebpackPlugin({
+                title: 'Development',
+                template: './src/index.html',
+                filename: 'index.html'
+            }),
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-    },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+        // turn off so we can see the source map for dom delegate so we can debug the library
+        devtool: 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: [
+                        /node_modules/,
+                    ],
+                },
+                {
+                    test: /\.html$/i,
+                    use: 'html-loader'
+                },
+                {
+                    test: /\.css$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: '[name][ext]'
+                    }
+                },
+                {
+                    test: /\.(png|svg)$/i,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'images/[name][ext]'
+                    }
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.svg'],
+        },
+        output: {
+            filename: '[name].js',
+            library: 'spsexample',
+            libraryTarget: 'umd',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true,
+            globalObject: 'this'
+        },
+        experiments: {
+            futureDefaults: true
+        },
+        optimization: {
+            minimize: false
+        },
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+        },
+    };
 };
