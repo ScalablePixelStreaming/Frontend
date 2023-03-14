c0ba7f37165d57847f045b9416e852d4daa30171
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-14 13:19:59 +1000

    Revert "updated the sps example webpack files with the new configureations"
    This reverts commit d99e9021f966a91f5712aee39bd544f36d8b94b5.

--- spsExample/webpack.config.js
+++ spsExample/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-//const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
 module.exports = (env) => {
