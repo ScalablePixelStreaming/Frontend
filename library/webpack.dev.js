f41c888932726a5d3a920cb4296165989804732a
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-28 10:14:09 +1000

    added base content to the dev and prod webpack files

---
+++ library/webpack.dev.js
@@ -0,0 +1,33 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const devCommon = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  }
+};
+
+module.exports = [
+  merge(common, devCommon, {
+    output: {
+      filename: 'libspsfrontend.js',
+      library: {
+        name: 'libspsfrontend', // exposed variable that will provide access to the library classes
+        type: 'umd'
+      },
+    },
+  }),
+  merge(common, devCommon, {
+    output: {
+      filename: 'libspsfrontend.esm.js',
+      library: {
+        type: 'module'
+      },
+    },
+    experiments: {
+      outputModule: true
+    }
+  })
+];
---
+++ library/webpack.prod.js
@@ -0,0 +1,34 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const prodCommon = {
+  mode: 'production',
+  optimization: {
+    usedExports: true,
+    minimize: true
+  },
+  stats: 'errors-only',
+};
+
+module.exports = [
+  merge(common, prodCommon, {
+    output: {
+      filename: 'libspsfrontend.js',
+      library: {
+        name: 'libspsfrontend', // exposed variable that will provide access to the library classes
+        type: 'umd'
+      },
+    },
+  }),
+  merge(common, prodCommon, {
+    output: {
+      filename: 'libspsfrontend.esm.js',
+      library: {
+        type: 'module'
+      },
+    },
+    experiments: {
+      outputModule: true
+    }
+  })
+];
