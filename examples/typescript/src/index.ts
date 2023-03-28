bfae2e49ff3fc50f589c526c85817c42e4c36b30
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-29 09:22:11 +1000

    updated the nade of the library variable in the example index.ts

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,18 +1,18 @@
-import * as spsFrontend from "@tensorworks/libspsfrontend";
+import * as libspsfrontend from "@tensorworks/libspsfrontend";
 
 // Apply default styling from Epic's frontend
-export const PixelStreamingApplicationStyles = new spsFrontend.PixelStreamingApplicationStyle();
+export const PixelStreamingApplicationStyles = new libspsfrontend.PixelStreamingApplicationStyle();
 PixelStreamingApplicationStyles.applyStyleSheet();
 
 document.body.onload = function () {
 
 	// Create a config object.
 	// Note: This config is extremely important, SPS only supports the browser sending the offer.
-	const config = new spsFrontend.Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
+	const config = new libspsfrontend.Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
 
 	// Create a Native DOM delegate instance that implements the Delegate interface class
-	const stream = new spsFrontend.PixelStreaming(config);
-	const spsApplication = new spsFrontend.SPSApplication({
+	const stream = new libspsfrontend.PixelStreaming(config);
+	const spsApplication = new libspsfrontend.SPSApplication({
 		stream,
 		onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode) /* Light/Dark mode support. */
 	});
