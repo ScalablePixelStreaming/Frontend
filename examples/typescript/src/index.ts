0cbcf882f39e1ca387c18253c8e24afa4fd0e7a4
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-30 09:49:37 +1000

    updated the index file to just import the required types

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,18 +1,18 @@
-import * as libspsfrontend from "@tensorworks/libspsfrontend";
+import {Config, PixelStreaming, SPSApplication, PixelStreamingApplicationStyle} from "@tensorworks/libspsfrontend";
 
 // Apply default styling from Epic Games Pixel Streaming Frontend
-export const PixelStreamingApplicationStyles = new libspsfrontend.PixelStreamingApplicationStyle();
+export const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
 PixelStreamingApplicationStyles.applyStyleSheet();
 
 document.body.onload = function () {
 
 	// Create a config object.
 	// Note: This config is extremely important, SPS only supports the browser sending the offer.
-	const config = new libspsfrontend.Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
+	const config = new Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
 
 	// Create stream and spsApplication instances that implement the Epic Games Pixel Streaming Frontend PixelStreaming and Application types
-	const stream = new libspsfrontend.PixelStreaming(config);
-	const spsApplication = new libspsfrontend.SPSApplication({
+	const stream = new PixelStreaming(config);
+	const spsApplication = new SPSApplication({
 		stream,
 		onColorModeChanged: (isLightMode) => PixelStreamingApplicationStyles.setColorMode(isLightMode) /* Light/Dark mode support. */
 	});
