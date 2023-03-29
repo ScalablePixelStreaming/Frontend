065c7fc83694abb633f7116d256c2df610211f6d
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-29 13:32:48 +1000

    updated webpack config to webpack common to make use of the dev and prod environments

---
+++ examples/typescript/webpack.common.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        index: './src/index.ts',
+    },
+    plugins: [
+        // new webpack.DefinePlugin({
+        //     WEBSOCKET_URL: JSON.stringify((env.WEBSOCKET_URL !== undefined) ? env.WEBSOCKET_URL : '')
+        // }),
+
+        new HtmlWebpackPlugin({
+            title: 'Scalable Pixel Streaming Frontend',
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: [
+                    /node_modules/,
+                ],
+            },
+            {
+                test: /\.html$/i,
+                use: 'html-loader'
+            },
+            {
+                test: /\.css$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: '[name][ext]'
+                }
+            },
+            {
+                test: /\.(png|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]'
+                }
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.svg'],
+    },
+    output: {
+        filename: '[name].js',
+        library: 'spstypescriptexample',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        globalObject: 'this',
+        hashFunction: 'xxhash64'
+    },
+    experiments: {
+        futureDefaults: true
+    },
+    optimization: {
+        minimize: false
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+    },
+};
