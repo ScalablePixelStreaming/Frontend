0e6eff2d1024c7c46e395f432ff5c83d8e283aa4
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-29 14:41:04 +1000

    updated comments in accordance to comment from luke post merge

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,6 +1,6 @@
 import * as libspsfrontend from "@tensorworks/libspsfrontend";
 
-// Apply default styling from Epic's frontend
+// Apply default styling from Epic Games Pixel Streaming Frontend
 export const PixelStreamingApplicationStyles = new libspsfrontend.PixelStreamingApplicationStyle();
 PixelStreamingApplicationStyles.applyStyleSheet();
 
@@ -10,7 +10,7 @@ document.body.onload = function () {
 	// Note: This config is extremely important, SPS only supports the browser sending the offer.
 	const config = new libspsfrontend.Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
 
-	// Create a Native DOM delegate instance that implements the Delegate interface class
+	// Create stream and spsApplication instances that implement the Epic Games Pixel Streaming Frontend PixelStreaming and Application types
 	const stream = new libspsfrontend.PixelStreaming(config);
 	const spsApplication = new libspsfrontend.SPSApplication({
 		stream,
