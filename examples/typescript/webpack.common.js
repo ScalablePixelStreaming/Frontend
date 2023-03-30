da665f89da4d6ad99b9ea6106b1bc754a5937966
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-30 12:11:34 +1000

    moved env file and renamed to example also included dontenv plugin to load env files

--- examples/typescript/webpack.common.js
+++ examples/typescript/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+require('dotenv').config({ path: './.env' }); 
 
 module.exports = {
     entry: {
