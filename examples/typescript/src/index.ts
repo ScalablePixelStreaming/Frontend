4bb88dfa2966f0ba90a0566fea1ff3245936e2a5
Adrian Zahra <adrian@tensorworks.com.au>
2023-03-30 12:21:37 +1000

    updated index file to make use of the env if it is not empty

--- examples/typescript/src/index.ts
+++ examples/typescript/src/index.ts
@@ -1,14 +1,23 @@
-import {Config, PixelStreaming, SPSApplication, PixelStreamingApplicationStyle} from "@tensorworks/libspsfrontend";
+import {Config, PixelStreaming, SPSApplication, TextParameters, PixelStreamingApplicationStyle} from "@tensorworks/libspsfrontend";
 
 // Apply default styling from Epic Games Pixel Streaming Frontend
 export const PixelStreamingApplicationStyles = new PixelStreamingApplicationStyle();
 PixelStreamingApplicationStyles.applyStyleSheet();
+
+// websocket url env
+declare var WEBSOCKET_URL: string;
 
 document.body.onload = function () {
 
 	// Create a config object.
 	// Note: This config is extremely important, SPS only supports the browser sending the offer.
 	const config = new Config({ useUrlParams: true, initialSettings: { OfferToReceive: true, TimeoutIfIdle: true } });
+
+	// make usage of WEBSOCKET_URL if it is not empty
+	let webSocketAddress = WEBSOCKET_URL;
+	if(webSocketAddress !== ""){
+		config.setTextSetting(TextParameters.SignallingServerUrl, webSocketAddress)
+	}
 
 	// Create stream and spsApplication instances that implement the Epic Games Pixel Streaming Frontend PixelStreaming and Application types
 	const stream = new PixelStreaming(config);
