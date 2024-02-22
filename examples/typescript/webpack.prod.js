7c042f1f8a1ec2c090ef801e09c4f8a8707424c9
David MacPherson <david@tensorworks.com.au>
2024-02-22 16:01:33 +1000

    Bug fix building for prod to exclude the WEBSOCKET_URL var stored in the .env file

--- examples/typescript/webpack.prod.js
+++ examples/typescript/webpack.prod.js
@@ -1,14 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
 
 module.exports = merge(common, {
-    mode: 'production',
-    optimization: {
-      usedExports: true,
-      minimize: true
-    },
-    stats: 'errors-only',
-	performance: {
-		hints: false
-	}
+  mode: 'production',
+  optimization: {
+    usedExports: true,
+    minimize: true
+  },
+  stats: 'errors-only',
+  performance: {
+    hints: false
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      WEBSOCKET_URL: undefined
+    }),
+  ]
 });
