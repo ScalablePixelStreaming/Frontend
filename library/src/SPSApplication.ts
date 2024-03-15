df09f428b4a5c21fb263c9950ed16f62c67eac68
Matthew Cotton <matt@tensorworks.com.au>
2024-03-15 14:09:57 +1100

    Moved out metrics reporting into its own class

--- library/src/SPSApplication.ts
+++ library/src/SPSApplication.ts
@@ -3,36 +3,16 @@ import { AggregatedStats, SettingFlag, TextParameters, Logger } from '@epicgames
 import { LoadingOverlay } from './LoadingOverlay';
 import { SPSSignalling } from './SignallingExtension';
 import { MessageStats } from './Messages';
-import { v4 as uuidv4 } from 'uuid';
+import { MetricsReporter } from './MetricsReporter';
 
 // For local testing. Declare a websocket URL that can be imported via a .env file that will override 
 // the signalling server URL builder.
 declare var WEBSOCKET_URL: string;
-declare var ENABLE_METRICS: boolean;
-declare var METRICS_URL: string;
-
-const SupportedStats : Record<string, string> = {
-	'video_width': 'width',
-	'video_height': 'height',
-	'video_bitrate': 'bitrate',
-	'video_dropped': 'dropped',
-	'video_packets_lost': 'packets lost',
-	'video_fps': 'fps',
-	'video_pli_count': 'count',
-	'video_keyframes': 'keyframes',
-	'video_nack_count': 'nacks',
-	'video_freeze_count': 'freeze',
-	'video_jitter': 'jitter',
-	'video_frame_count': 'frame count',
-	'audio_bitrate': 'audio bitrate'
-}
 
 export class SPSApplication extends Application {
 	private loadingOverlay: LoadingOverlay;
 	private signallingExtension: SPSSignalling;
-	private statsTimer: ReturnType<typeof setInterval> | undefined;
-	private sessionData: any;
-	private loadingStart: number | undefined;
+	private metricsReporter: MetricsReporter;
 
 	static Flags = class {
 		static sendToServer = "sendStatsToServer"
@@ -59,151 +39,20 @@ export class SPSApplication extends Application {
 		spsSettingsSection.appendChild(new SettingUIFlag(sendStatsToServerSetting).rootElement);
 		this.loadingOverlay = new LoadingOverlay(this.stream.videoElementParent);
 
+		this.metricsReporter = new MetricsReporter();
 		this.stream.addEventListener(
 			'statsReceived',
 			({ data: { aggregatedStats } }) => {
-				this.onSessionStats(aggregatedStats);
+				this.metricsReporter.onSessionStats(aggregatedStats);
 				if (sendStatsToServerSetting.flag) {
 					this.sendStatsToSignallingServer(aggregatedStats);
 				}
 			}
 		);
 
-		this.stream.addEventListener('webRtcConnected', () => this.startSession() );
+		this.stream.addEventListener('webRtcConnected', () => this.metricsReporter.startSession() );
 	}
 
-	startSession() {
-		if (!ENABLE_METRICS) {
-			return;
-		}
-
-		// generate a unique session id
-		const sessionId: string = uuidv4();
-
-		// register end of session event
-		window.addEventListener('beforeunload', () => this.endSession(sessionId));
-
-		// collect some session data
-		this.sessionData = {};
-		this.sessionData.id = sessionId;
-		this.sessionData.startTime = Date.now();
-		this.sessionData.userAgent = navigator.userAgent;
-
-		if (this.loadingStart) {
-			this.sessionData.loadingDuration = this.sessionData.startTime - this.loadingStart;
-			this.loadingStart = undefined;
-		}
-
-		// stop the stats polling if it exists for some reason
-		if (this.statsTimer) {
-			clearInterval(this.statsTimer);
-		}
-	}
-
-	updateStatValue(name: string, value: number) {
-		if (!ENABLE_METRICS) {
-			return;
-		}
-
-		if (value == null) {
-			return;
-		}
-
-		const statDescription = SupportedStats[name];
-		if (!statDescription) {
-			console.log(`unknown stat ${name}`);
-			return;
-		}
-
-		if (!this.sessionData) {
-			console.log(`no sessiondata`);
-			return;
-		}
-
-		if (!this.sessionData.stats) {
-			this.sessionData.stats = {};
-		}
-
-		if (this.sessionData.stats[name]) {
-			this.sessionData.stats[name].value = value;
-		} else {
-			this.sessionData.stats[name] = {
-				description: statDescription,
-				value: value
-			};
-		}
-	}
-
-	onSessionStats(aggregatedStats: AggregatedStats) {
-		if (!ENABLE_METRICS) {
-			return;
-		}
-
-		if (!this.sessionData) {
-			return;
-		}
-
-		this.sessionData.lastSent = Date.now();
-
-		// if sessionData is defined we can assume the session is active
-		if (aggregatedStats.inboundVideoStats) {
-			const videoStats = aggregatedStats.inboundVideoStats;
-			this.updateStatValue("video_width", videoStats.frameWidth);
-			this.updateStatValue("video_height", videoStats.frameHeight);
-			this.updateStatValue("video_bitrate", videoStats.bitrate);
-			this.updateStatValue("video_dropped", videoStats.framesDropped);
-			this.updateStatValue("video_packets_lost", videoStats.packetsLost);
-			// rtt?
-			this.updateStatValue("video_fps", videoStats.framesPerSecond);
-			this.updateStatValue("video_pli_count", videoStats.pliCount);
-			this.updateStatValue("video_keyframes", videoStats.keyFramesDecoded);
-			this.updateStatValue("video_nack_count", videoStats.nackCount);
-			this.updateStatValue("video_freeze_count", videoStats.freezeCount);
-			this.updateStatValue("video_jitter", videoStats.jitter);
-			this.updateStatValue("video_frame_count", videoStats.framesReceived);
-		}
-		if (aggregatedStats.inboundAudioStats) {
-			const audioStats = aggregatedStats.inboundAudioStats;
-			this.updateStatValue("audio_bitrate", audioStats.bitrate);
-		}
-
-		// POST the stats
-		this.httpPost(this.sessionData.stats);
-	}
-
-	async httpPost(data: any) {
-		const post_data = {
-			id: this.sessionData.id,
-			metrics: data
-		};
-
-		const metrics_url = METRICS_URL || `http://${window.location.hostname}:8000/stats`;
-		try {
-			const response = await fetch(metrics_url, {
-				method: 'POST',
-				mode: "no-cors",
-				body: JSON.stringify(post_data),
-				headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-			});
-		} catch (error) {
-			console.error(`Unable to POST stats to ${metrics_url}: ${error}`);
-		}
-	}
-
-	endSession(sessionId: string) {
-		if (!ENABLE_METRICS) {
-			return;
-		}
-
-		// record end time
-		this.sessionData.endTime = Date.now();
-		this.sessionData.duration = this.sessionData.endTime - this.sessionData.startTime;
-
-		// send session data
-		
-		// clear session stats which also indicates no session
-		this.sessionData = undefined;
-	}
 
 	handleSignallingResponse(signallingResp: string, isError: boolean) {
 		if (isError) {
@@ -242,10 +91,7 @@ export class SPSApplication extends Application {
 
 		this.currentOverlay = this.loadingOverlay;
 
-		// only record the first call to loading.
-		if (!this.loadingStart) {
-			this.loadingStart = Date.now();
-		}
+		this.metricsReporter.startLoading();
 	}
 
 	/**
@@ -257,3 +103,4 @@ export class SPSApplication extends Application {
 		this.stream.webSocketController.webSocket.send(data.payload());
 	}
 }
+
